f78f98cc66aa985ad85824cf97244d86c930db5f
Aaron Peikert <aaron.peikert@posteo.de>
2022-01-21 14:18:45 +0100

    include syntax in modul

--- src/Semi.jl
+++ src/Semi.jl
@@ -1,14 +1,16 @@
 module Semi
 
-export Node
-export SimpleNode
-export Edge
-export DirectedEdge
-export UndirectedEdge
-export Modifier
-export ModifiedEdge
-export ModifiedNode
+    export Node
+    export SimpleNode
+    export Edge
+    export DirectedEdge
+    export UndirectedEdge
+    export Modifier
+    export ModifiedEdge
+    export ModifiedNode
 
-include("graph_types.jl")
+    include("graph_types.jl")
+    include("syntax/symbol.jl")
+    include("syntax/edge.jl")
 
 end
