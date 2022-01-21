269fd16c5e058211d9ba3facc87d4a5ae8145d76
Aaron Peikert <aaron.peikert@posteo.de>
2022-01-21 12:12:41 +0100

    export all types

--- src/Semi.jl
+++ src/Semi.jl
@@ -1,6 +1,13 @@
 module Semi
 
 export Node
+export SimpleNode
+export Edge
+export DirectedEdge
+export UndirectedEdge
+export Modifier
+export ModifiedEdge
+export ModifiedNode
 
 include("graph_types.jl")
 
