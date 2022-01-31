65d5b861660fc3598338a47487aab2c92ea81323
Documenter.jl <documenter@juliadocs.github.io>
2022-01-31 08:15:36 +0000

    build based on ab538be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StenoGraphs.jl","page":"StenoGraphs.jl","title":"StenoGraphs.jl","text":"","category":"section"},{"location":"","page":"StenoGraphs.jl","title":"StenoGraphs.jl","text":"Documentation for StenoGraphs.jl","category":"page"}]
+}
