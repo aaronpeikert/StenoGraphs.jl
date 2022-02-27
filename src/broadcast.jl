6b7a715a1cf16f3441aa787f2d1888f2dad6ed77
Aaron Peikert <aaron.peikert@posteo.de>
2022-02-27 07:58:09 +0100

    broadcast arrows

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -1,4 +1,4 @@
-edges = (:DirectedEdge, :UndirectedEdge)
+edges = (:DirectedEdge, :UndirectedEdge, :→, :←, :↔)
 for e in edges
     @eval $e(src::VecOrMat, dst::VecOrMat) = vec([$e(l, r) for r in dst, l in src])
     @eval $e(src::Node, dst::VecOrMat) = vec(broadcast($e, (src,), dst))
