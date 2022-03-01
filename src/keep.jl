2d869c1902127c470690c34a06c64b3dcb3a3ceb
Aaron Peikert <aaron.peikert@posteo.de>
2022-03-01 18:50:30 +0100

    add some more methods for keep

--- src/keep.jl
+++ src/keep.jl
@@ -8,9 +8,12 @@ struct Dst <: SourceDestination end
 
 keep(x::AbstractNode, ::Type{T}) where {T <: Node} = keep(x.node, T)
 keep(x::Node, ::Type{T}) where {T <: Node} = x
+keep(x::Node, ::Type{T}) where {T <: AbstractNode} = x
 
 keep(x::AbstractEdge, ::Type{T}) where {T <: Edge} = keep(x.edge, T)
 keep(x::Edge, ::Type{T}) where {T <: Edge} = x
+keep(x::Edge, ::Type{T}) where {T <: AbstractNode} = x
+
 keep(x::AbstractEdge, ::Type{T}) where {T <: Src} = x.src
 keep(x::AbstractEdge, ::Type{T}) where {T <: Dst} = x.dst
 
@@ -20,6 +23,10 @@ keep(x::Arrow, ::Type{T}) where {T <: Edge} = keep(x.edge, T)
 keep(x::Arrow, ::Type{T}) where {T <: Left} = x.lhs
 keep(x::Arrow, ::Type{T}) where {T <: Right} = x.rhs
 
+function keep(x::AbstractNode, ::Type{T1}, ::Type{T2}) where {T1 <: Union{Side, SourceDestination}, T2 <: AbstractNode}
+    keep(x, T2)
+end
+
 function keep(x::Arrow, ::Type{T1}, ::Type{T2}) where {T1 <: Union{Side, SourceDestination}, T2 <: AbstractNode}
     keep(keep(x, T1), T2)
 end
@@ -27,7 +34,7 @@ end
 keep(x::Arrow, ::Type{T}) where {T <: Src} = keep(x.edge, T)
 keep(x::Arrow, ::Type{T}) where {T <: Dst} = keep(x.edge, T)
 
-keep(x::Arrow, ::Type{T}) where {T <: Arrow} = x
+keep(x::Arrow, _) = x
 
 unmeta(x::AbstractEdge) = keep(x, Edge)
 unmeta(x::AbstractNode) = keep(x, Node)
