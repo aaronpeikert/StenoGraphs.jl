b8829177881d0db843139744ed2e42c64dd5021c
Aaron Peikert <aaron.peikert@posteo.de>
2022-03-17 09:31:59 +0100

    throw for >1 argument for `_(...)`

--- src/syntax/symbol.jl
+++ src/syntax/symbol.jl
@@ -11,6 +11,7 @@ function quote_symbols(ex::Expr)
 
     if ex.head == :call
         if ex.args[1] == :_ 
+            length(ex.args) > 2 ? error("Unqote only a single argument. Right: `_(x)`, Wrong: `_(x, y)`.") : nothing
             return ex.args[2]
         end
         to_quote = to_quote[2:end]
--- test/syntax.jl
+++ test/syntax.jl
@@ -2,6 +2,15 @@
     @test (:a → :b) == (StenoGraphs.@quote_symbols a → b)
     b = :c
     @test (:a → :c) == (StenoGraphs.@quote_symbols a → _(b))
+    let err = nothing
+        try
+            eval(:(StenoGraphs.@quote_symbols a → _(b, c)))
+        catch err
+        end
+    
+        @test err isa Exception
+        @test  occursin("Unqote only a single argument. Right: `_(x)`, Wrong: `_(x, y)`.", sprint(showerror, err))
+    end
 end
 
 @testset "Addition as hcat" begin
