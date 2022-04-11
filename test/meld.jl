d996ea6de92bff966065be2af382c6c166a850f3
Aaron Peikert <aaron.peikert@posteo.de>
2022-04-11 21:10:50 +0200

    increase coverage

--- test/meld.jl
+++ test/meld.jl
@@ -19,11 +19,14 @@ end
 
     @test merge(e1, e2) == e2
     @test merge(e2, e3) == e2 * Start(4)
+    e4 = Edge(Node(:a), Node(:c))
+    @test_throws StenoGraphs.EdgeMismatch merge(e1, e4)
 end
 
 @testset "Merge Nodes" begin
     n1 = Node(:a)
     n2 = n1 ^ Label("hi")
+    @test merge(n1, n1) == n1
     @test merge(n1, n2) == n2
     @test merge(n2, n2) == n2
 
@@ -32,4 +35,28 @@ end
     @test merge(n1, n3) == n3
     @test merge(n3, n4) == n3 * Start(1)
     @test_throws StenoGraphs.NodeMismatch merge(n2, n3)
+
+    n5 = Node(:b) * Weight(1)
+    @test_throws StenoGraphs.NodeMismatch merge(n1, n5)
+end
+
+@testset "Merge Nodes" begin
+    let err = nothing
+        try
+            eval(:(merge(n2, n3)))
+        catch err
+        end
+    
+        @test err isa Exception
+        @test  occursin("$n1 ≠ $n3", sprint(showerror, err))
+    end
+    let err = nothing
+        try
+            eval(:(merge(e1, e4)))
+        catch err
+        end
+    
+        @test err isa Exception
+        @test  occursin("$e1 ≠ $e4", sprint(showerror, err))
+    end
 end
