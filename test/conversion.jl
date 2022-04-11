316e83bd3e4a1fb46b162af5d8bb4ec6282d6962
Aaron Peikert <aaron.peikert@posteo.de>
2022-04-11 18:04:19 +0200

    promote meta edges/nodes

--- test/conversion.jl
+++ test/conversion.jl
@@ -1,3 +1,4 @@
+using StenoGraphs
 @testset "Conversion" begin
     @test convert(Node,:a) == Node(:a)
 end
@@ -6,9 +7,20 @@ struct Start{S <: Number} <: EdgeModifier
     s::S
 end
 
+struct Label <: NodeModifier
+    l
+end
+
 @testset "Promotion" begin
     @test eltype([Node(:a), :b]) <: Node
     @test eltype([Node(:a), :b, Start(1) * Node(:c)]) <: AbstractNode
     @test eltype([:b, Start(1) * Node(:c)]) <: AbstractNode
     @test eltype([Start(1) * Node(:b), Start(1) * Node(:c)]) <: MetaNode
+
+    @test eltype(promote(Node(:a), ModifiedNode(Node(:b), Label("hi")))) <: ModifiedNode
+    ns = Node.([:a, :b]) .^ Ref(Label("test"))
+    ns[1] = Node(:a)
+    @test eltype(ns) <: ModifiedNode
+
+    @test eltype([ModifiedEdge(Edge(Node(:a), Node(:b)), Start(3.0)), Edge(Node(:a), Node(:c))]) <: ModifiedEdge
 end
