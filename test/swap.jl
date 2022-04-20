684791b00f048b040bdce008aec7f2f042bf7a26
Aaron Peikert <aaron.peikert@posteo.de>
2022-04-20 10:48:41 +0200

    add test pairswap

--- test/swap.jl
+++ test/swap.jl
@@ -7,8 +7,8 @@ struct Observed <: NodeModifier end
 @testset "Swap Node <-> Node" begin
     n1 = Node(:a)
     n2 = Node(:b)
-    @test swap_node(n1, n1, n2) == StenoGraphs.swap_node(n1, n1 => n2) == n2
-    @test swap_node(n1 ^ Observed(), n1, n2) == n2 ^ Observed()
+    @test swap_node(n1, n1, n2) == swap_node(n1, n1 => n2) == n2
+    @test swap_node(n1 ^ Observed(), n1, n2) == swap_node(n1 ^ Observed(), n1 => n2) == n2 ^ Observed()
     @test swap_node(n1 * Weight(1), n1, n2) == n2 * Weight(1)
 end
 
