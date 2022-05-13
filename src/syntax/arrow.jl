7a061f8029353822195ae3524e5aee09247b078e
Aaron Peikert <aaron.peikert@posteo.de>
2022-05-13 12:45:39 +0200

    add docstrings for arrows

--- src/syntax/arrow.jl
+++ src/syntax/arrow.jl
@@ -15,11 +15,112 @@ function Arrow(lhs, rhs, src, dst; edge_fun = DirectedEdge, cross = false)
     Arrow(edges, lhs, rhs)
 end
 
+"""
+Single (broadcasting) arrow right (\\rightarrow, Alt Gr + i)
+
+```jldoctest
+julia> @StenoGraph a → b
+a → b
+
+julia> eltype(ans)
+DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] → [c d]
+a → c
+b → d
+```
+"""
 →(lhs, rhs) = Arrow(lhs, rhs, keep(lhs, Right, Node), keep(rhs, Left, AbstractNode))
+
+"""
+Double (cross product) arrow right (\\Rightarrow)
+
+```jldoctest
+julia> @StenoGraph a ⇒ b
+a → b
+
+julia> eltype(ans)
+DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] ⇒ [c d]
+a → c
+a → d
+b → c
+b → d
+```
+"""
 ⇒(lhs, rhs) = Arrow(lhs, rhs, keep(lhs, Right, Node), keep(rhs, Left, AbstractNode); cross = true)
+
+"""
+Single (broadcasting) arrow left (\\leftarrow, Alt Gr + z)
+
+```jldoctest
+julia> @StenoGraph a ← b
+b → a
+
+julia> eltype(ans)
+DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] ← [c d]
+c → a
+d → b
+```
+"""
 ←(lhs, rhs) = Arrow(lhs, rhs, keep(rhs, Left, Node), keep(lhs, Right, AbstractNode))
+
+"""
+Double (cross product) arrow left (\\Leftarrow)
+
+```jldoctest
+julia> @StenoGraph a ⇐ b
+b → a
+
+julia> eltype(ans)
+DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] ⇐ [c d]
+c → a
+c → b
+d → a
+d → b
+```
+"""
 ⇐(lhs, rhs) = Arrow(lhs, rhs, keep(rhs, Left, Node), keep(lhs, Right, AbstractNode); cross = true)
+
+"""
+Single (broadcasting) arrow left-right (\\leftrightarrow)
+
+```jldoctest
+julia> @StenoGraph a ↔ b
+a ↔ b
+
+julia> eltype(ans)
+UndirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] ↔ [c d]
+a ↔ c
+b ↔ d
+```
+"""
 ↔(lhs, rhs) = Arrow(lhs, rhs, keep(lhs, Right, Node), keep(rhs, Left, AbstractNode); edge_fun = UndirectedEdge)
+
+"""
+Double (cross product) arrow left-right (\\Leftrightarrow)
+
+```jldoctest
+julia> @StenoGraph a ⇔ b
+a ↔ b
+
+julia> eltype(ans)
+UndirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}
+
+julia> @StenoGraph [a b] ⇔ [c d]
+a ↔ c
+a ↔ d
+b ↔ c
+b ↔ d
+```
+"""
 ⇔(lhs, rhs) = Arrow(lhs, rhs, keep(lhs, Right, Node), keep(rhs, Left, AbstractNode); edge_fun = UndirectedEdge, cross = true)
 
 # if either lhs or rhs are a single node, broadcast arrow (barrow) behave like cross arrow (carrow)
