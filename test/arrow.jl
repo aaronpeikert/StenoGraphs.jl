1e89ced2b91ffbc02e7d560fecb4ee6d3edc25be
Aaron Peikert <aaron.peikert@posteo.de>
2022-05-17 11:36:02 +0200

    make test more readable

--- test/arrow.jl
+++ test/arrow.jl
@@ -45,7 +45,7 @@ end
 @testset "Multiple Nodes Chain" begin
     bresult = [Edge(Node(:a), Node(:c)); Edge(Node(:b), Node(:d)); Edge(Node(:c), Node(:e)); Edge(Node(:d), Node(:f)); Edge(Node(:e), Node(:g)); Edge(Node(:f), Node(:h))]
     cresult = [Edge(Node(:a), Node(:c)); Edge(Node(:b), Node(:d)); Edge(Node(:c), Node(:e)); Edge(Node(:d), Node(:f)); Edge(Node(:e), Node(:g)); Edge(Node(:f), Node(:h));
-    Edge(:b, :c); Edge(:a, :d); Edge(:d, :e); Edge(:c, :f); Edge(:f, :g); Edge(:e, :h)]
+    Edge(Node(:b), Node(:c)); Edge(Node(:a), Node(:d)); Edge(Node(:d), Node(:e)); Edge(Node(:c), Node(:f)); Edge(Node(:f), Node(:g)); Edge(Node(:e), Node(:h))]
 
     @test unarrow(Node.([:a :b]) → Node.([:c :d]) → Node.([:e :f]) → Node.([:g :h])) ⊆ bresult
     @test unarrow(Node.([:a :b]) ← Node.([:c :d]) ← Node.([:e :f]) ← Node.([:g :h])) ⊆ switch.(bresult)
@@ -68,13 +68,13 @@ end
 @testset "Modified  Chain" begin
     # correct placement of modifiers
     result = ModifiedEdge(Edge(Node(:a), Node(:b)), [Start(4)])
-    @test unarrow(Node(:a) → Start(4)*:b)[1] == result
+    @test unarrow(Node(:a) → Start(4)*Node(:b))[1] == result
     @test unarrow(Start(4) * Node(:a) ← Node(:b))[1] == switch(result)
     @test unarrow(Node(:a) ↔ Node(:b) * Start(4))[1] == undirect(result)
 
-    #incorrect
-    @test unarrow(Start(4) * Node(:a) → Node(:b))[1] == result.edge
-    @test unarrow(Node(:a) ← Node(:b) * Start(4))[1] == switch(result).edge
-    @test unarrow(Start(4) * Node(:a) ↔ Node(:b))[1] == undirect(result).edge
+    #incorrect placed modifiers are silently ignored
+    @test unarrow(Start(4) * Node(:a) → Node(:b))[1] == unmeta(result)
+    @test unarrow(Node(:a) ← Node(:b) * Start(4))[1] == unmeta(switch(result))
+    @test unarrow(Start(4) * Node(:a) ↔ Node(:b))[1] == unmeta(undirect(result))
 
 end
