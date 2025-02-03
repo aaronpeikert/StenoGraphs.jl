65d1ef9e6950ff4713180ca1311ccd952a33a470
Aaron Peikert <aaron.peikert@posteo.de>
2025-02-03 18:59:35 +0100

    forgot hygiene

--- src/syntax/variable_as_node.jl
+++ src/syntax/variable_as_node.jl
@@ -12,7 +12,7 @@ function variable_as_node!(ex::Expr, node::Type{T} where {T <: AbstractNode})
     if ex.head == :call
         if ex.args[1] == :_ 
             length(ex.args) > 2 ? error("Unqote only a single argument. Right: `_(x)`, Wrong: `_(x, y)`.") : nothing
-            return Expr(:call, :(StenoGraphs.convert_symbol), ex.args[2], :SimpleNode)
+            return Expr(:call, :(StenoGraphs.convert_symbol), ex.args[2], :(StenoGraphs.SimpleNode))
         end
         to_quote = to_quote[2:end]
     end
